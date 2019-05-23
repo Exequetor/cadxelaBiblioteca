a2bb021ee8dc19321981d8ec63f181e3c3feb948
moy01 <m.o.i.s.e.s20@hotmail.com>
2019-05-22 21:57:25 -0500

    Update Adeudos_Controller.php

--- application/controllers/Adeudos_Controller.php
+++ application/controllers/Adeudos_Controller.php
@@ -14,6 +14,7 @@ class Adeudos_Controller extends CI_Controller{
 		/**
 		* Cargamos el modelo de adeudos
 		**/
+		$this->load->view("templates/header");
 		$this->load->model('Adeudos_Model', 'AM', true);
 	}
 	public function index(){
