febc6082f79d21bbc0082fdde7953a7c62095adc
moy01 <m.o.i.s.e.s20@hotmail.com>
2019-05-22 21:50:29 -0500

    MVC de buscador de adeudos por libro o matricula
    Se implemento una funcion en el contolador  Adeudos_Controller
    Se implemento una funcion en el modelo  Adeudos_Model
    Se añadio una nueva vista Adeudos

--- application/models/Adeudos_Model.php
+++ application/models/Adeudos_Model.php
@@ -107,4 +107,29 @@ class Adeudos_Model extends CI_Model{
 			}
 		}
 	}
+
+	/*
+	By moises Veha Hernández 
+	Conulta que devuelve los datos de un adeudo buscando por matricula o libro 
+	*/
+	public function ver_adeudo(string $Radeudo)
+	{
+		//$this->db->like('matricula_estudiante',$Radeudo);
+		//$this->db->or_like('matricula',$Radeudo);
+		//$Radeudo=$this->db->get('adeudos');
+		return $this->db->query("SELECT * FROM 
+(
+    SELECT libros.titulo,estudiantes.nombre,adeudos.matricula_estudiante,adeudos.fecha_reposicion
+    FROM adeudos
+    INNER JOIN estudiantes ON  estudiantes.matricula=adeudos.matricula_estudiante
+    INNER JOIN libros ON libros.id_libro=adeudos.id_libro
+) as R_adeudo
+WHERE R_adeudo.titulo='{$Radeudo}' or  R_adeudo.matricula_estudiante='{$Radeudo}'")->result();
+		if ($Radeudo->num_rows()>0) {
+			return $Radeudo->result();
+		}else{
+			return false;
+		}
+	}
 }
+
